e1b6239b18865af296f13b8a692c0fc3435a4516
yamaguchi-chie <yamagu.webfun2304@gmail.com>
2024-01-17 13:15:41 +0900

    FAQ ブロック要素

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,24 @@
+!(function () {
+    const viewport = document.querySelector('meta[name="viewport"]');
+    function switchViewport() {
+      const value =
+        window.outerWidth > 375
+          ? 'width=device-width,initial-scale=1'
+          : 'width=360';
+      if (viewport.getAttribute('content') !== value) {
+        viewport.setAttribute('content', value);
+      }
+    }
+    addEventListener('resize', switchViewport, false);
+    switchViewport();
+  })();
 
 jQuery(function ($) { // この中であればWordpressでも「$」が使用可能になる
-
+//よくある質問 / SPコラム詳細 アコーディオン//
+$('.accordion__q').on('click', function() {
+    $(this).next('.accordion__a').slideToggle();
+    $(this).toggleClass('js-open');
+    $(this).next('.accordion__a').toggleClass('js-open');
+});
 
 });
