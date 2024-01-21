1b6acd369dd7cfb5d8d4327e80909b3aaaaf8d04
transpacificflight <transpacific.flight@gmail.com>
2024-01-21 17:06:42 +0900

    スライダーの修正

--- src/js/script.js
+++ src/js/script.js
@@ -56,53 +56,25 @@ var Swiper1 = new Swiper('.swiper-top', {
   },
 });
 
-
-document.addEventListener('DOMContentLoaded', function() {
-  var Swiper2;
-  // 有効化時のオプションを記述
-  function initializeSwiper() {
-      Swiper2 = new Swiper('.swiper-cars', {
-        loop: true,
-        loopAdditionalSlides: 1,
-        speed: 500,
-        grabCursor: true,
-        /*autoplay: { 
-          delay: 3000, 
-          disableOnInteraction: false,
-          waitForTransition: false,
-        },*/
-        centeredSlides: true, // アクティブなスライドを中央にする
-        slidesPerView: "auto",// autoにする
-        pagination: {
-            el: ".slide--car .swiper-pagination",
-            clickable: true,
-            type: 'bullets'
-        },
-        navigation: {
-            nextEl: ".slide--car .swiper-button-next",
-            prevEl: ".slide--car .swiper-button-prev"
-        },
-    });
-  }
-  // 無効化する用の記述
-  function destroySwiper() {
-      if (Swiper2) {
-          Swiper2.destroy();
-          Swiper2 = undefined;
-      }
-  }
-  // if分を用いて横幅に応じて関数を実行
-  function handleResize() {
-      var w = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
-      if (w < 768) {
-          if (!Swiper2) {
-              initializeSwiper();
-          }
-      } else {
-          destroySwiper();
-      }
-  }
-  // 初回実行
-  handleResize();
-  window.addEventListener('resize', handleResize);
+var Swiper2 = new Swiper('.swiper-cars', {
+  loop: true,
+  loopAdditionalSlides: 1,
+  speed: 500,
+  grabCursor: true,
+  /*autoplay: { 
+    delay: 3000, 
+    disableOnInteraction: false,
+    waitForTransition: false,
+  },*/
+  centeredSlides: true, // アクティブなスライドを中央にする
+  slidesPerView: "auto",// autoにする
+  pagination: {
+      el: ".slide--car .swiper-pagination",
+      clickable: true,
+      type: 'bullets'
+  },
+  navigation: {
+      nextEl: ".slide--car .swiper-button-next",
+      prevEl: ".slide--car .swiper-button-prev"
+  },
 });
